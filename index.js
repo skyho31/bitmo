fe8cd18cdeb0225fb614787140c5ed838a6e30c9
Seonho Kim <shkim@n3n.co.kr>
2018-02-07 06:28:03 +0900

    re-arranged bot

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var dataCollector = require('./dataCollector');
 var marketChecker = require('./marketChecker');
 var common = require('./common').event;
-var bot = require('./bitmobot/ubmo_4.7_alpha.3');
-// var bot = require('./bitmobot/ubmo_4.7');
+var bot = require('./bitmobot/bitmo_alpha');
+// var bot = require('./bitmobot/testmo_alpha');
 var intervalTime = 5 * 1000;
 
 dataCollector.init(common, intervalTime);
